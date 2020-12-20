7424230796037413fadd39907fc9e0ebe89db620
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-12-20 00:52:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_arr = []
   array.each do |arr|
-    squre_num = arr ** arr 
+    squre_num = arr ** 2
     new_arr << squre_num 
     return new_arr
   end
